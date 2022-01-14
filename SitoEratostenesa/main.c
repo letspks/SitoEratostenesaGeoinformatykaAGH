81bcbef91312d1318195b95c168ccd100fc7e9cc
letspks <96194211+letspks@users.noreply.github.com>
2022-01-14 17:27:14 +0100

    Update main.c

--- SitoEratostenesa/main.c
+++ SitoEratostenesa/main.c
@@ -10,34 +10,48 @@ int main()
     int q;
 
     printf("Algorytm wypisujacy liczby pierwsze mniejsze od N metoda sita Eratostenesa\n");
-    printf("Podaj N:\n");
-    scanf("%d", &n);
-    printf("Podales liczbe:\n");
-    printf("%d", n);
-    printf("\n");
-    int liczby[n];
-    int liczbygotowe[n];
-    for(int i=0; i<n; i=i+1)
+    FILE *plik = fopen("nte.txt", "r");
+    printf("Plik powinien zawierac 10 zmiennych\n");
+    if(plik==NULL)
     {
-        liczby[i]=1+i;
-        liczbygotowe[i]=1+i;
+        perror("NIE MOZNA ZLOKALIZOWAC PLIKU NTE\n");
+        exit(0);
     }
-    printf("\n");
-    for(int i=1; liczbygotowe[i]*liczbygotowe[i]<=n; i=i+1)
-    {
-        for(int q=2; liczbygotowe[i]*q<=n; q=q+1)
-        {
-            liczby[(liczby[i]*q)-1]=0;
-        }
-    }
-    printf("Liczby pierwsze mniejsze od podanego N to:\n");
-    for(int i=0; i<n; i=i+1)
-    {
-        if(liczby[i]!=0)
-        {
-            printf("%d", liczbygotowe[i]);
-            printf("\n");
-        }
+    printf("Plik otwarty pomyslnie\n");
+    int tabx[10];
 
+
+    for(int k=0; k<10; k=k+1)
+    {
+        n=tabx[k];
+        printf("Obliczanie dla liczby:");
+        printf("%d", n);
+
+        int liczby[n];
+        int liczbygotowe[n];
+
+        for(int i=0; i<n; i=i+1)
+        {
+            liczby[i]=1+i;
+            liczbygotowe[i]=1+i;
+        }
+        printf("\n");
+        for(int i=1; liczbygotowe[i]*liczbygotowe[i]<=n; i=i+1)
+        {
+            for(int q=2; liczbygotowe[i]*q<=n; q=q+1)
+            {
+                liczby[(liczby[i]*q)-1]=0;
+            }
+        }
+        printf("Liczby pierwsze mniejsze od podanego N to:\n");
+        for(int i=0; i<n; i=i+1)
+        {
+            if(liczby[i]!=0)
+            {
+                printf("%d", liczbygotowe[i]);
+                printf("\n");
+            }
+
+        }
     }
 }
